fcfe1a9f3e0fc9820de0edd9f5ae72924ed79f80
Hellyson-Ferreira <hellysonferreira.hf1@gmail.com>
2020-07-17 14:34:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,26 @@ from selenium import webdriver
 from time import sleep
 
 
+URL = "https://www.instagram.com/p/CCpHERaFpm6/"
+USERNAME = "XXXXXXXXXXXX"
+PASSWORD = "xxxxxxxxxxxxxxxxx"
+COMMENT_NUMBER = 1000
 
-Url = "http://XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+COMMENT_LIST = [
+    '@felipeanders.on__',
+    '@ciellen_santos',
+    '@francinete.franca.16',
+    '@francianesantos1607',
+    '@magda_36.1',
+    '@ytaciria',
+    '@antoniaantoniabrandao',
 
-driver = webdriver.Chrome()
-instagram = Instagram(driver)
+]
+USER_NUMBER_PER_COMMENT = 2
+
+DRIVER = webdriver.Chrome()
+
+instagram = Instagram(DRIVER, URL)
+instagram.navigate()
+instagram.login(USERNAME, PASSWORD)
+instagram.comment(COMMENT_LIST, USER_NUMBER_PER_COMMENT, COMMENT_NUMBER)
