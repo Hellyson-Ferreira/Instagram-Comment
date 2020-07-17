a1201a0663523a71f7b29e366d705f16772b1492
Hellyson-Ferreira <hellysonferreira.hf1@gmail.com>
2020-07-17 14:34:36 -0300

    métodos para manipular a página do instagram e escolher os usuarios

--- instagram.py
+++ instagram.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from time import sleep
-from random import choice
+from random import choice,randrange
 
 
 class Instagram:
@@ -11,43 +11,53 @@ class Instagram:
         self.__urlIntagramPost = urlIntagramPost
 
         # Elements of the intagram login page #xPath
-        self.__InputUserName = '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/div/Input/input'
-        self.__InputPassword = '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input'
+        self.__InputUserName = 'username'  # name
+        self.__InputPassword = 'password'  # name
+        # xPath
         self.__BtnLogin = '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button'
 
         # Elements of the intagram post page #xPath
 
-        self.__inputComment = '//*[@id="react-root"]/section/main/div/div/article/div[3]/section[3]/div/form/textarea'
+        self.__inputComment = 'Ypffh'  # class
         self.__btnPostComment = '//*[@id="react-root"]/section/main/div/div/article/div[3]/section[3]/div/form/button'
 
     def navigate(self):
-        self.driver.get(self.urlIntagram)
+        self.__driver.get(self.__urlIntagram)
         sleep(2)
 
     def login(self, userName, password):
-        self.driver.find_element_xpath(
+        print("Login Intagram")
+        self.__driver.find_element_by_name(
             self.__InputUserName).send_keys(userName)
-        self.driver.find_element_xpath(
+        self.__driver.find_element_by_name(
             self.__InputPassword).send_keys(password)
-        self.driver.find_element_xpath(self.__BtnLogin).click()
+        self.__driver.find_element_by_xpath(self.__BtnLogin).click()
         sleep(2)
+        self.__driver.get(self.__urlIntagramPost)
+        sleep(4)
 
-    def userNamePost(self, commentsList, indexList, commentNumberPerComment=1):
+    def __choiceusersNamePost(self, commentsList, userNumberPerComment):
+        commentsList = commentsList[:]
+        comments = []
+        while len(comments) < userNumberPerComment:
+            commnet = choice(commentsList)
+            comments.append(commnet)
+            commentsList.remove(commnet)
+        return comments
 
-        if userNumberPerComment == 1:
-            return indexList
-        else:
-            listIndex = [index]
-            for elementIndex in range(commentNumberPerComment):
-                if indexList > (len(commentList) - 1):  
-                    indexList = 0
+    def comment(self, commentsList, userNumberPerComment, commentNumber=1):
+        print("Comment")
+        for i in range(commentNumber):
+            index = 0
+            choiceusersNamePost = self.__choiceusersNamePost(
+                commentsList, userNumberPerComment)
+            self.__driver.find_element_by_class_name(
+                    self.__inputComment).click()
+            for element in choiceusersNamePost:
 
-                listIndex.append(index + 1)
-
-    def comment(self, commentsList, commentNumberPerComment=1, commentNumber=1):
-        indexList = 0
-        for i in range(numberComment):
-            
-            self.driver.find_element_xpath(self.__inputComment).send_keys()
-            self.driver.find_element_xpath(self.__btnPostComment).click()
-            
+                self.__driver.find_element_by_class_name(
+                    self.__inputComment).send_keys(f'{element} ')
+            sleep(2)
+            self.__driver.find_element_by_xpath(self.__btnPostComment).click()
+            print(f'Comentario de numero {i+1} => {choiceusersNamePost}')
+            sleep(60+randrange(0, 9))
