5349ac102a5c0c8d83b83f750b891b0b392df49e
Hellyson-Ferreira <hellysonferreira.hf1@gmail.com>
2020-09-11 08:19:33 -0300

    USer list

--- main.py
+++ main.py
@@ -1,33 +1,22 @@
 from instagram import Instagram
 from selenium import webdriver
 from time import sleep
+from comment_list import COMMENT_LIST
 
-#886
-URL = "https://www.instagram.com/p/CDML-F1h_ib/"
-USERNAME = "XXXXXXX"
-PASSWORD = "XXXXXXXXXXX"
+#219 + 
+URL = "https://www.instagram.com/p/CCZfJc9jbFz/"
+USERNAME = "gabrielnascimento1514"
+PASSWORD = "XXXXXXXXXXXXX"
 COMMENT_NUMBER = 1000
-COMMENT_LIST = [
-    '@adriel.victrre',
-    '@felipeanders.on__',
-    '@franferreira1023',
-    '@joao.vvi',
-    '@__hellysonferreira__',
-    '@ray_santtory',
-    '@livia_magalhaesbkl',
-    '@ayslanguima',
-    '@basilioelcio',
-    '@felipeanderson__',
-    '@luaneisabele82',
-    '@joseemanuelsame',
-    '@joseemanuel778',
-]
 
-USER_NUMBER_PER_COMMENT = 1
+COMMENT_LIST = COMMENT_LIST
+
+USER_NUMBER_PER_COMMENT = 2
 
 DRIVER = webdriver.Chrome()
 
-instagram = Instagram(DRIVER, URL)
-instagram.navigate()
-instagram.login(USERNAME, PASSWORD)
-instagram.comment(COMMENT_LIST, USER_NUMBER_PER_COMMENT, COMMENT_NUMBER)
+INSTAGRAM = Instagram(DRIVER, URL)
+INSTAGRAM.navigate()
+INSTAGRAM.login(USERNAME, PASSWORD)
+INSTAGRAM.comment(COMMENT_LIST, USER_NUMBER_PER_COMMENT, COMMENT_NUMBER)
+
